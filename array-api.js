0c1225213b38f128e9f6e60650286781b8b7ce7f
gilbert9172 <gilbert9172@naver.com>
2022-03-03 23:51:42 +0900

    22.03.03

---
+++ array-api.js
@@ -0,0 +1,10 @@
+// Q1. make a string out og an array
+const fruits1 = ['apple','banana','orage']
+const result1 = fruits1.join('.');
+console.log("result1 = ", result1);
+
+// Q2. make an array out of a String
+
+const fruits2 = 'apple,banana,orane';
+const result2 = fruits2.split(',');
+console.log("result2 = ", result2);
---
+++ function.js
@@ -0,0 +1,5 @@
+let today = new Date();
+
+let month = Number(today.getMonth() + 1);
+
+console.log(month);
